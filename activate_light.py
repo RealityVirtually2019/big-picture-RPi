6cac943531233b8f8fb3f76f4885482ed7e5ad35
Albert Schweitzer <schweitzer.albert@gmail.com>
2019-01-20 02:38:28 -0500

    fake friends

--- activate_light.py
+++ activate_light.py
@@ -1,6 +1,12 @@
 import RPi.GPIO as GPIO
 import time
+import http.client
+
 while True:
+  conn = http.client.HTTPSConnection('https://vsb5spplil.execute-api.us-east-1.amazonaws.com/prod/status?device_id=light1', 443)
+  conn.request('GET', '/') # <---
+  r = conn.getresponse()
+  print(r.read())
   GPIO.setmode(GPIO.BCM)
   GPIO.setwarnings(False)
   GPIO.setup(11,GPIO.OUT)
